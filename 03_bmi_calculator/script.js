abcd69701bfff2d391f71974a64e859f188122e7
simplysaadali <ali091g@gmail.com>
2026-09-01 19:09:09 +0500

    more enhancements and conditions applied, more changes

--- 03_bmi_calculator/script.js
+++ 03_bmi_calculator/script.js
@@ -1,18 +1,38 @@
 const form = document.querySelector("form");
 
-form.addEventListener("submit", function(e){
+form.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    const height = parseInt(document.querySelector("#height").value)
-    const weight = parseInt(document.querySelector("#weight").value)
+    const height = parseFloat(document.querySelector("#height").value);
+    const weight = parseFloat(document.querySelector("#weight").value);
     const results = document.querySelector("#results");
 
-    if(height === "" || height < 0 || isNaN(height)){
+    if (isNaN(height) || height <= 0 || height === "") {
         results.innerHTML = "Please enter a valid height";
-    }else if(weight === "" || weight < 0 || isNaN(weight)){
+        results.style.color = "red";
+        return;
+    }
+
+    if (isNaN(weight) || weight <= 0 || weight === "") {
         results.innerHTML = "Please enter a valid weight";
-    }else{
-        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-        results.innerHTML = `Your BMI is ${bmi}`;
+        results.style.color = "red";
+        return;
+    }
+
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    results.innerHTML = `Your BMI is ${bmi}`;
+
+    if (Number(bmi) < 18.5) {
+        results.innerHTML += "<br>You're underweight";
+        results.style.color = "red";
+    } else if (Number(bmi) >= 18.5 && Number(bmi) <= 24.9) {
+        results.innerHTML += "<br>You're healthy";
+        results.style.color = "green";
+    } else if (Number(bmi) >= 25 && Number(bmi) <= 29.9) {
+        results.innerHTML += "<br>You're overweight";
+        results.style.color = "orange";
+    } else {
+        results.innerHTML += "<br>You're obese";
+        results.style.color = "red";
     }
 });
